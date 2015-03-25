7598be787dd7239452c97df270a1ee8b88af2993
Paulo Henrique Leite <paulo-http@live.com>
2015-03-25 19:51:17 -0300

    topBar

---
+++ weAppLoginWebService/logoutViewController.h
@@ -0,0 +1,13 @@
+//
+//  logoutViewController.h
+//  weAppLoginWebService
+//
+//  Created by Paulo Henrique Leite on 3/25/15.
+//  Copyright (c) 2015 Humberto Vieira de Castro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface logoutViewController : UIViewController
+
+@end
---
+++ weAppLoginWebService/logoutViewController.m
@@ -0,0 +1,43 @@
+//
+//  logoutViewController.m
+//  weAppLoginWebService
+//
+//  Created by Paulo Henrique Leite on 3/25/15.
+//  Copyright (c) 2015 Humberto Vieira de Castro. All rights reserved.
+//
+
+#import "logoutViewController.h"
+
+@interface logoutViewController ()
+
+@end
+
+@implementation logoutViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    UIStoryboard * tela = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    UINavigationController * view = [tela instantiateViewControllerWithIdentifier:@"rootID"];
+    view.modalTransitionStyle = UIModalTransitionStyleCoverVertical;
+    [self presentViewController:view animated:YES completion:nil];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
