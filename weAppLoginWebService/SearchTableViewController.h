303624391d8a91f485e4d9095d9e2c9469576c84
Paulo Henrique Leite <paulo-http@live.com>
2015-03-26 10:32:12 -0300

    search implemented

---
+++ weAppLoginWebService/SearchTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchTableViewController.h
+//  weAppLoginWebService
+//
+//  Created by Laurana Franco on 26/03/15.
+//  Copyright (c) 2015 Humberto Vieira de Castro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchTableViewController : UITableViewController
+
+@end
